91233994b6a7952ba4f38651f6c7392abfef7213
Михаил Кузьмич <michalkuzmicz87@gmail.com>
2023-04-03 15:06:49 +0200

    fix some route problems

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes} from 'react-router-dom';
 import { Navigation } from './components/Navigation';
 import { FavoritesPage } from './pages/FavoritesPage';
 import { HomePage } from './pages/HomePage';
@@ -11,7 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/favorites" element={<FavoritesPage />} />
-      </Routes>
+    </Routes>
     </>
   );
 }
