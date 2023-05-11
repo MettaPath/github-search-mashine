e83f9edaa26dff63afb482dce3aadc691b0ddf61
Михаил Кузьмич <michalkuzmicz87@gmail.com>
2023-05-11 19:50:17 +0200

    clear App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { Navigation } from './components/Navigation';
 import { FavoritesPage } from './pages/FavoritesPage';
 import { HomePage } from './pages/HomePage';
@@ -10,27 +10,10 @@ import { ResetPass } from './components/ResetPass';
 import { Profile } from './components/Profile';
 
 function App() {
-	const location = useLocation();
-	const [previousPath, setPreviousPath] = useState('');
-
-	useEffect(() => {
-		if (previousPath !== location.pathname) {
-			setPreviousPath(location.pathname);
-			localStorage.setItem('previousPath', location.pathname);
-		}
-	}, [location.pathname, previousPath]);
-
-	useEffect(() => {
-		const previousPath = localStorage.getItem('previousPath');
-		if (previousPath && previousPath !== '/') {
-			setPreviousPath(previousPath);
-		}
-	}, []);
-
 	return (
 		<>
 			<Navigation />
-			<Routes location={previousPath || location}>
+			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="/signup" element={<RegistrationForm />} />
 				<Route path="/login" element={<Login />} />
